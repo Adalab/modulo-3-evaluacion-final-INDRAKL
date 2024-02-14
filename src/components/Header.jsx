a54844cfd08a7089baf039b88f0c3a8b81051ebc
Indra Kishinchand López <indrakislo@gmail.com>
2024-02-14 21:20:39 +0100

    header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import HarryPotter from "../images/harrypotter.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="header__photo">
-      <img src={HarryPotter} alt="Harry Potter" />
-    </div>
+    <Link to="/">
+      <div className="header__photo">
+        <img src={HarryPotter} alt="Harry Potter" href="" />
+      </div>
+    </Link>
   );
 }
 
